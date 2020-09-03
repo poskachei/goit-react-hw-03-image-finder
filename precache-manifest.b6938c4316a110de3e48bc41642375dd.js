279e716020ffb7a61f3e0ab918cc79a0366cd3cc
poskachei <vposkachey@gmail.com>
2020-09-03 10:34:20 +0300

    Updates

---
+++ precache-manifest.b6938c4316a110de3e48bc41642375dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33074e103539215b660377c3e8e0bb7c",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "a89211ff7b71e1f52dfa",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.365b677b.chunk.css"
+  },
+  {
+    "revision": "ffcbfa4eda7d69067d2f",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.50509a29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.50509a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a89211ff7b71e1f52dfa",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.93a75f56.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
